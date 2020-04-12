328ae6c662cbeea0bbcd27aaf91aec4760fffa7d
thabiso40 <thabisofakude40@gmail.comm>
2020-04-12 17:17:19 +0200

    import project

--- RSVP/RSVP/Controllers/HomeController.cs
+++ RSVP/RSVP/Controllers/HomeController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RSVP.Models;
+
 
 namespace RSVP.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ViewResult Index()
         {
+            int hours = DateTime.Now.Hour;
+            ViewBag.greeting = hours < 12 ? "Good morning" : "Good Afternoon";
             return View();
+        }
+        [HttpGet]
+        public ActionResult RsvpForm() {
+
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+
+                else {
+                    // there is a validation error
+                    return View();
+                }
         }
     }
 }
--- RSVP/RSVP/Properties/AssemblyInfo.cs
+++ RSVP/RSVP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RSVP")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("915013a4-cb7f-4720-aecc-db19825da310")]
+[assembly: Guid("fde22756-485e-49b0-9d03-48343acd9053")]
 
 // Version information for an assembly consists of the following four values:
 //
